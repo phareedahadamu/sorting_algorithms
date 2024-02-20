2ac6927af0622f374e484387fb0e31f3afeae2f9
phareedahadamu <phareedahadamu@gmail.com>
2024-02-20 23:37:27 +0100

    Task 0 fix?

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,25 +10,27 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, n = size;
-	int tmp;
+	int tmp, check;
 
-	if (array != NULL && size > 1)
-	{
-		for (i = 0; i < (size - 1); i++)
-		{
-			for (j = 0; j < (n - 1); j++)
-			{
-				if (array[j] > array[j + 1])
-				{
-					tmp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = tmp;
-					print_array(array, size);
-				}
-			}
-			n--;
-		}
-	}
-	else
+	if (array == NULL || size <= 1)
 		return;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		check = 0;
+		for (j = 0; j < (n - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+				check = 1;
+			}
+		}
+		if (check == 0)
+			break;
+		n--;
+	}
 }
