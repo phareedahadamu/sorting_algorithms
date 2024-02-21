7243f5319ef794b966d4e64fc64d36d52b39add8
phareedahadamu <phareedahadamu@gmail.com>
2024-02-21 12:54:24 +0100

    Task 0 fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,16 +9,15 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, n = size;
+	size_t i, j;
 	int tmp, check;
 
 	if (array == NULL || size <= 1)
 		return;
-
 	for (i = 0; i < (size - 1); i++)
 	{
 		check = 0;
-		for (j = 0; j < (n - 1); j++)
+		for (j = 0; j < (size - 1 - i); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -31,6 +30,5 @@ void bubble_sort(int *array, size_t size)
 		}
 		if (check == 0)
 			break;
-		n--;
 	}
 }
